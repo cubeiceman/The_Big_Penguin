69d6f2413801e171482c2f1b255e4e74ea491de6
Henry (Hyeoncheol) Yang <88568778+TowlTheOwl@users.noreply.github.com>
2024-03-03 13:33:16 -0800

    Delete Hackathon-main directory

--- Hackathon-main/Functions.py
+++
@@ -1,3 +0,0 @@
-def in_rect(points, rectangle):
-    return (rectangle.rect.x < points[0] < rectangle.rect.x + rectangle.rect.width) and (
-            rectangle.rect.y < points[1] < rectangle.rect.y + rectangle.rect.height)
